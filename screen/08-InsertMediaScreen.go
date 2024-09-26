41c8e24d66d06305772a2391cf6479f70daf9d9b
Ivan Kuznetsov <jsvapiav@gmail.com>
2024-09-26 11:35:42 +0000

    do check fs fix
    Signed-off-by: Ivan Kuznetsov <jsvapiav@gmail.com>

--- screen/08-InsertMediaScreen.go
+++ screen/08-InsertMediaScreen.go
@@ -157,7 +157,7 @@ func SelectOption() string {
 }
 
 func resizeRootFs(disk string, mountPoint string) {
-	var resizeCmd = "(echo d; echo 2; echo n; echo 2; echo ''; echo ''; echo w;) | sudo fdisk " + disk // + "\nsudo e2fsck -f " + disk + "p2" + "\nsudo resize2fs " + disk + "p2"
+	var resizeCmd = "(echo d; echo 2; echo n; echo 2; echo ''; echo ''; echo w;) | sudo fdisk " + disk + "\nsudo e2fsck -f " + disk + "p2" + "\nsudo resize2fs " + disk + "p2"
 	umountMedia(mountPoint)
 
 	// Write the string to the file
@@ -177,7 +177,7 @@ func resizeRootFs(disk string, mountPoint string) {
 		panic(er)
 	}
 
-	if true {
+	if false {
 		pterm.Info.Printfln("sudo e2fsck -f %s", disk + "p2")
 		msg, er = global.ExecCommand("sudo", "e2fsck", "-f ", disk + "p2")
 		for _, m := range msg {
